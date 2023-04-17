619173cfdd8629301dffd27102f7a0b744836f75
anaijg <ana.jimenezgomez@educa.madrid.org>
2023-04-17 21:11:20 +0200

    Ejemplos iterador y mapa

--- src/ejemploMapas/Ejemplo1.java
+++ src/ejemploMapas/Ejemplo1.java
@@ -11,7 +11,7 @@ import java.util.Iterator;
 public class Ejemplo1 {
     public static void main(String[] args) {
         // Creamos un mapa
-        // K (clave): idSocio
+        // K (clave): id
         // V (valor): nombre
         HashMap<Integer, String> mapa = new HashMap<>();
         // 1) Añadir elementos
@@ -20,7 +20,22 @@ public class Ejemplo1 {
         mapa.put(3, "Periquito");       
         mapa.put(4, "Victorito");
         // 2) Recuperar elementos: le doy la clave para que me recupere el valor
-        System.out.println(mapa.get(3));
+        System.out.println(mapa.get(3)); // 3 es la clave, no la posición
         System.out.println(mapa.get(2));
+        // 3) Si queremos hacer algo en función de que exista la clave o no: containsKey()
+        if (mapa.containsKey(5)) {
+            System.out.println(mapa.get(5));
+        } else {
+            System.out.println("No existe la clave indicada.");
+        }
+        // 4) Eliminar una entrada de un mapa
+        if (mapa.containsKey(4)) {
+            mapa.remove(4);
+        }
+        if (mapa.containsKey(4)) {
+            System.out.println(mapa.get(4));
+        } else {
+            System.out.println("La clave no existe");
+        }
     }
 }
